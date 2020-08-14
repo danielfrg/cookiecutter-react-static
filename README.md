3618022ee89c04e4ef8706c01b5543e40e2c8498
Daniel Rodriguez <daniel@danielfrg.com>
2020-08-14 14:37:31 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# cookiecutter-react-static
+
+A template for a static website, just HTML, plus React for some interactivity.
+
+Other features:
+
+- Bootstrap
+- Webpack
+- SASS
---
+++ js/src/components/App/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./style.scss";
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <h3>This content comes from React!</h3>;
+    }
+}
+
+export default App;
